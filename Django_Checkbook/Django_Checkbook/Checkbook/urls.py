97d73128ec5785aee55cd0ec1a6431a4d7804445
wilkmi24 <70912258+wilkmi24@users.noreply.github.com>
2020-11-29 15:20:11 -0800

    uploading to GitHub

--- Django_Checkbook/Django_Checkbook/Checkbook/urls.py
+++ Django_Checkbook/Django_Checkbook/Checkbook/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpattersn = [
     path('', views.home, name='index'),
     path('create/', views.create_account, name='create'),
-    path('balance/', views.balance, name='balance',
+    path('<int:pk>/balance/', views.balance, name='balance',
     path('transaction/', views.transaction, name='transaction'),
 ]
