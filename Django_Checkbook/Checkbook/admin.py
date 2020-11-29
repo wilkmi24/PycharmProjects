cc24bc5bd7067a8a1a81d2189c83d12fbe19c6cc
wilkmi24 <70912258+wilkmi24@users.noreply.github.com>
2020-11-29 14:35:35 -0800

    uplading to GitHub

--- Django_Checkbook/Checkbook/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import account
-
-admin.site.register(Account)
-
-from .models import transaction
-
-admin.site.register(Transaction)
--- Django_Checkbook/Checkbook/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CheckbookConfig(AppConfig):
-    name = 'Checkbook'
